2bdca9d129b48a644e1b796f95b8b680912166b8
DR <toonzmy@gmail.com>
2021-05-25 16:52:35 -0400

    Initial commit

---
+++ app/src/main/java/com/dj/android29demo1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dj.android29demo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dj.android30demo1.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
